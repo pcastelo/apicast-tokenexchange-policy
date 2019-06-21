d447619c85dca9f14441b1310cad789d1d60fec6
Pablo Castelo <pablo@nalabs.io>
2019-06-21 17:19:24 -0300

    fix policiy

--- policies/token_exchange/0.1/token_exchange.lua
+++ policies/token_exchange/0.1/token_exchange.lua
@@ -39,8 +39,8 @@ local function exchange_token(self)
 end
 
 function _M:access(context)
-    ngx.log(ngx.INFO ,'CONTEXT:',context)
-    ngx.log(ngx.INFO ,'SELF:',self)
+    --ngx.log(ngx.INFO ,'CONTEXT:',context)
+    --ngx.log(ngx.INFO ,'SELF:',self)
     ngx.log(ngx.INFO ,'REFERERR:',self.referer)
     exchange_token(self)
 end
